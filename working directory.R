9eba6ece01aeb9adfabf6a2f62cfef454fbe2dca
Cy-Cai <cyruschoi10@gmail.com>
2019-08-28 08:05:24 -0400

    PULL murders.csv data to working directory

--- working directory.R
+++ working directory.R
@@ -1,1 +1,11 @@
 library(dslabs)
+path<-system.file("extdata",package="dslabs")
+list.files(path)
+filename<-"murders.csv"
+fullpath<-file.path(path,filename)
+fullpath
+setwd("C:/Users/a7qsazz/Documents/Data Science/Data Wrangling/projects/murders")
+getwd()
+file.copy(fullpath,getwd())
+
+
